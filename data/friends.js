354626d74bd893b1b09c0aff0ece213abba0906b
chuckt <chtbootcamp@gmail.com>
2018-08-11 22:56:43 -0700

    first try

---
+++ data/friends.js
@@ -0,0 +1,59 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the friends survey results.
+// ===============================================================================
+
+var friendsArray = [{
+  name: "Ahmed",
+  photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  scores: [1,2,1,2,1,2,1,2,1,2]
+},
+{
+  name: "Ted",
+  photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  scores: [5,4,3,2,1,5,4,3,2,1]
+},
+{name: "Fred",
+  photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  scores: [4,5,4,5,4,5,4,5,4,5]
+},
+{name: "Bob",
+  photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  scores: [4,4,4,4,4,4,4,4,4,4]
+},
+{name: "Scott",
+  photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  scores: [1,5,1,5,1,5,1,5,1,5]
+},
+{name: "Elvis",
+  photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  scores: [2,4,2,4,2,4,2,4,2,4]
+},
+{name: "Jimmy",
+  photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  scores: [0,0,0,0,0,0,0,0,0,0]
+},
+{name: "Alfred",
+  photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  scores: [3,3,3,3,3,3,3,3,3,3]
+},
+{name: "Eric",
+  photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  scores: [1,2,4,3,2,4,3,1,0,3]
+},
+{name: "Jack",
+  photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  scores: [1,2,3,2,5,5,4,3,4,2]
+},
+{name: "Roger",
+  photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  scores: [2,3,2,3,2,3,2,4,3,4]
+},
+{name: "Tim",
+  photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  scores: [2,2,2,2,2,2,2,2,2,2]
+},
+];
+
+// make it accessible to other files using require.
+module.exports = friendsArray;
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,24 @@
+// ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+var path = require("path");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // HTML GET Requests
+  // ---------------------------------------------------------------------------
+
+  app.get("/servey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/servey.html"));
+  });
+
+  // If no matching route is found default to home
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+};
